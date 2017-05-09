dd0385b5eed83cb5a7737c754e33c15662f1dfd5
Chris Carr <coder.knot@gmail.com>
2017-05-09 16:47:03 -0700

    add firebase as data source instead of mock albums file

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
 import { Album } from '../album.model';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
@@ -11,7 +12,7 @@ import { Album } from '../album.model';
 })
 
 export class MarketplaceComponent implements OnInit {
-  albums: Album[];
+  albums: FirebaseListObservable<any[]>;
 
   constructor(private router: Router, private albumService: AlbumService){}
 
